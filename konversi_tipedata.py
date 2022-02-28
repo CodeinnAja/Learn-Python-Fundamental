4023b6a5f727295a02f7166d8a41dd951506d7ca
JokoArdh <jokoardianto12@gmail.com>
2022-02-28 12:21:57 +0700

    konversi tipedata dan mengenal tipe data

---
+++ konversi_tipedata.py
@@ -0,0 +1,14 @@
+#mengubah atau konversi tipe data
+
+data = 20
+print(data)
+
+#konversi ke float
+dataFloat = float(data)
+print(dataFloat)
+print(type(dataFloat))
+
+#konversi sting
+dataStr = str(data)
+print(dataStr)
+print(type(dataStr))
---
+++ tipe_data.py
@@ -0,0 +1,42 @@
+#mengenal berbagai macam tipe data yang ada dalam
+#bahasa pemrograman python
+
+#tipe data boolean
+print(True)
+print(False)
+
+#tipe data string
+print("Hello, saya joko ardianto sedang belajar python")
+
+#tipe data interger
+print(20)
+
+#tipe data float
+print(3.14)
+
+#tipe data hexadecimal
+hex(50)
+
+#tipe data complex
+print(5j)
+
+#tipe data list
+print([1,2,3,4,5])
+print(["satu", "dua", "tiga"])
+
+#tipe data tupple
+print((1,2,3,4))
+print(("ari","angga","syifa"))
+
+#tipe data dictionary
+print({"nama": "Joko", "umur": 23})
+biodata = {"nama": "Joko", "umur": 23} #ketika digunakan sebagai variabel
+print(biodata)
+
+#untuk mengecek jenis tipe ata
+print(type(biodata))
+
+nilai1 = 1.234
+nilai2 = 20
+print(type(nilai1))
+print(type(nilai2))
