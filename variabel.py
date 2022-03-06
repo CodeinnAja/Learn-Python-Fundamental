ffaae54ab9ee1a43088a6a5309ed370cdd050d20
joko <c20010007@uby.ac.id>
2022-03-06 10:42:46 +0700

    variabel

---
+++ variabel.py
@@ -0,0 +1,18 @@
+#contoh penulisan variabel dalam python
+
+Fnama = "Joko"
+Lnama = "Ardiyanto"
+fullnama = Fnama + " " + Lnama
+umur = "23 tahun"
+status = "Mahasiswa"
+
+print(fullnama)  #untuk menampilkan nilai suatu vvariabel
+print(fullnama, "\n", umur, "\n", status)
+
+#example
+panjang = 5
+lebar = 7
+luas = panjang * lebar
+print(luas)
+
+
