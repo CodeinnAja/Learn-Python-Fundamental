7952796892ce2ee0dc828f088217a8d18139d17b
joko <c20010007@uby.ac.id>
2022-03-06 11:01:47 +0700

    program sederhana dengan variabel string

---
+++ variabel_ex.py
@@ -0,0 +1,15 @@
+#membuat program biodata dengan variabel
+
+print("=====================================")
+print("   PROGRAM BIODATA DENGAN  PYTHON    ")
+print("=====================================\n")
+
+nama = input("Masukan nama anda : ")
+umur = input("Masukan umur anda : ")
+addres = input("Masukan alamat  : ")
+hobi = input("Masukan hobi anda : ")
+print("\n Otput :")
+print("Halo saya %s, umur %s tahun, alamat %s dan hobi saya %s" %(nama, umur, addres, hobi))
+
+
+
