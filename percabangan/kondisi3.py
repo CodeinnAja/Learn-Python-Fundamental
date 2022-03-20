138408ccafdd24052a0b6e5bcea16b70bff55210
JokoArdh <jokoardianto12@gmail.com>
2022-03-20 11:12:48 +0700

    kondisi percabngan elif

---
+++ percabangan/kondisi3.py
@@ -0,0 +1,16 @@
+hari = input("pelajaran apa yang kamu lakukan pada hari : ")
+
+if(hari == "senin"):
+    print("saya belajar Digital Forensic & Cyber Security")
+elif(hari == "selasa"):
+    print("saya belajar Reakayasa perangkat Lunak")
+elif(hari == "rabu"):
+    print("saya belajar Pemrograman berbasis Web & Statistika")
+elif(hari == "kamis"):
+    print("saya belajar Jaringan digital & Teknologi Wireless")
+elif(hari == "jumat"):
+    print("saya belajar computer VIsion & Internetworking")
+elif(hari == "sabtu"):
+    print("saya belajar AR & Data mining")
+else:
+    print("saya libur")
