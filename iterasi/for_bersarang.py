afd80e586f643377abc17ae38aaa5e0a143856ee
JokoArdh <jokoardianto12@gmail.com>
2022-03-25 20:10:55 +0700

    for bersarang

---
+++ iterasi/for_bersarang.py
@@ -0,0 +1,9 @@
+#perulangan bersarang pada python
+list_angka = [1,2]
+list_huruf = ['a','b']
+
+for angka in  list_angka:
+    for huruf in list_huruf:
+
+        print(huruf)
+        print(angka)
