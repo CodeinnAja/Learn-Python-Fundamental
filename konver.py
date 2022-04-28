12847cf998d69f2a02efe2e238b9e4693af86d37
JokoArdh <jokoardianto12@gmail.com>
2022-04-28 21:15:30 +0700

    membuat example list

---
+++ konver.py
@@ -0,0 +1,5 @@
+angka = int('10')/2 # mengubah string menjadi  bilang bulat dan dibagi 2
+print(angka)
+
+hello = "I like Number " + str(9) #ubah angka 9 menjadi string dan menggunakan operator +
+print(hello)
---
+++ tupple list/list.py
@@ -0,0 +1,9 @@
+# contoh lis dengan berbagai karakter tipe data
+
+bilangan = [1, 2, 3, 4, 5]
+nama = ["aldy", "fahrudin", "tiara", "eka"]
+info = ["eka", 25, "13 cm"]
+
+print(bilangan)
+print(nama)
+print(info)
